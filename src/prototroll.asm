c08d5d91a2352dd36c3b271e970b71f248f719fe
Parrot user <user@localhost.localdomain>
2021-01-16 23:37:09 +0000

    style: clean const arguments

--- src/prototroll.asm
+++ src/prototroll.asm
@@ -1,10 +1,27 @@
 %include        'utils/functions.asm'
 
+SECTION .rodata
+    SYS_FORK            equ 2
+    SYS_READ            equ 3
+    SYS_WRITE           equ 4
+    SYS_CLOSE           equ 6
+    SYS_SOCKETCALL      equ 102
+
+    SKT_SOCKET          equ 1
+    SKT_BIND            equ 2
+    SKT_LISTEN          equ 4
+    SKT_ACCEPT          equ 5
+
+    PF_INET             equ 2
+    SOCK_STREAM         equ 1
+    IPPROTO_TCP         equ 6
+    AF_INET             equ 2
+
 SECTION .data
-invite      db      'Say something: ', 0h
+    invite      db      'Say something: ', 0h
 
 SECTION .bss
-buffer      resb    255
+    buffer      resb    255
 
 SECTION .text
 global _start
@@ -16,34 +33,34 @@ _start:
     xor     esi, esi
 
 _socket:
-    push    byte 6              ; IPPROTO_TCP
-    push    byte 1              ; SOCK_STREAM
-    push    byte 2              ; PF_INET
+    push    byte IPPROTO_TCP
+    push    byte SOCK_STREAM
+    push    byte PF_INET
     mov     ecx, esp
-    mov     ebx, 1              ; subroutine SOCKET(1)
-    mov     eax, 102            ; SYS_SOCKETCALL
+    mov     ebx, SKT_SOCKET
+    mov     eax, SYS_SOCKETCALL
     int     80h
 
 _bind:
     mov     edi, eax
     push    dword 0x00000000    ; ip address: 0.0.0.0
     push    word 0x2923         ; port: 9001
-    push    word 2              ; AF_INET
+    push    word AF_INET
     mov     ecx, esp
     push    byte 16             ; arg len
     push    ecx
     push    edi
     mov     ecx, esp
-    mov     ebx, 2              ; subroutine BIND(2)
-    mov     eax, 102
+    mov     ebx, SKT_BIND
+    mov     eax, SYS_SOCKETCALL
     int     80h
 
 _listen:
     push    byte 1              ; max queue len argument
     push    edi
     mov     ecx, esp
-    mov     ebx, 4              ; subroutine LISTEN(4)
-    mov     eax, 102
+    mov     ebx, SKT_LISTEN
+    mov     eax, SYS_SOCKETCALL
     int     80h
 
 _accept:
@@ -51,13 +68,13 @@ _accept:
     push    byte 0              ; address argument
     push    edi
     mov     ecx, esp
-    mov     ebx, 5              ; subroutine ACCEPT(5)
-    mov     eax, 102
+    mov     ebx, SKT_ACCEPT
+    mov     eax, SYS_SOCKETCALL
     int     80h
 
 _fork:
     mov     esi, eax
-    mov     eax, 2              ; SYS_FORK
+    mov     eax, SYS_FORK
     int     80h
 
     cmp     eax, 0
@@ -69,7 +86,7 @@ _writeInvite:
     mov     edx, 16
     mov     ecx, invite
     mov     ebx, esi
-    mov     eax, 4
+    mov     eax, SYS_WRITE
     int     80h
 
 _cleanBuffer:
@@ -89,24 +106,24 @@ _read:
     mov     edx, 255
     mov     ecx, buffer
     mov     ebx, esi
-    mov     eax, 3              ; SYS_READ
+    mov     eax, SYS_READ
     int     80h
 
     mov     eax, buffer
-    call    sprintLF
+    call    sprint
 
 _write:
     mov     edx, 78
     mov     ecx, buffer
     mov     ebx, esi
-    mov     eax, 4
+    mov     eax, SYS_WRITE
     int     80h
 
     jmp     _writeInvite
 
 _close:
     mov     ebx, esi
-    mov     eax, 6              ; SYS_CLOSE
+    mov     eax, SYS_CLOSE
     int     80h
 
 _exit:
